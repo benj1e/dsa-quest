8b9cba24c441dc492290879328c3937e2f3e4bbf
Benjamin Alimele <benjaminali968@gmail.com>
2024-12-28 01:06:36 -0400

    Moving to a new workspace *wink

--- dsa_quest/app/views.py
+++ dsa_quest/app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def landing_page(request):
+    """Landing page view"""
+    return render(request, "app/landing_page.html")
--- dsa_quest/dsa_quest/settings.py
+++ dsa_quest/dsa_quest/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "app",
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = "dsa_quest.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
